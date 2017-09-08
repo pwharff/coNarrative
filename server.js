dfebbd38b294d77907ddf871710962d44308ab06
Unknown <pwharff@gmail.com>
2017-09-08 12:10:33 -0700

    Added node version to package.json

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
+const {doSomething1, doSomething2} = require("./doSomething.js")
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-
 const db = {
-    creditcards: []
+    creditCards: []
 }
 
 
@@ -23,8 +23,10 @@ app.get('/new', function (req, res) {
     })
 })
 
-app.get('/creditcards', function (req, res) {
-    res.send(db.creditcards);
+app.get('/creditCards', function (req, res) {
+    res.send(db.creditCards)
+    doSomething1()
+    doSomething2()
 })
 
 app.put('/save', bodyParser.json(), function (req, res) {
@@ -37,3 +39,4 @@ app.put('/save', bodyParser.json(), function (req, res) {
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
+
